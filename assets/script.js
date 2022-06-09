06d2caf2998e92c5b44eb705391d02a1d7abe2b0
thabata <thabatagcampos@gmail.com>
2022-06-09 19:54:56 -0300

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+const exibir = document.querySelector("#listaImagens");
+const button = document.querySelector("#readmore");
+
+button.onclick = function () {
+    if (exibir.className == "open") { 
+      // ler menos
+      exibir.className = "";  
+      button.innerHTML = "Mostrar mais"; // Ativar o botão mostrar mais
+    } else {
+      // ler mais
+      exibir.className = "open"; // se a class estiver open
+      button.innerHTML = "Mostrar menos"; // Trocar o botão 
+    }
+ };
+
+
+//  button.onclick = function () {
+//   if (exibir.className != "open") { 
+//    // ler mais
+//    exibir.className = "open"; // se a class estiver open
+//    button.innerHTML = "Mostrar menos"; // Mostrar o botão mostrar menos
+//   } else {
+//      // ler menos
+//      exibir.className = "";  
+//      button.innerHTML = "Mostrar mais"; // Mostrar mais
+//   }
+// };
+
+
+// button.addEventListener("click", readMore);
+
+// function readMore() {
+//      if (exibir.className == "open") {
+//        // Ler menos
+//      exibir.className = "";
+//      button.innerHTML = "Mostrar mais"; // Mostrar mais
+//    } else {
+//      exibir.className = "open";
+//      button.innerHTML = "Mostrar menos"; // Mostrar menos
+//    }
+// }
