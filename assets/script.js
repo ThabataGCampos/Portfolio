36f70ddbbcae2139df5edad85f41c13ab633c2d1
thabata <thabatagcampos@gmail.com>
2022-06-10 16:15:59 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,25 @@
-const exibir = document.querySelector("#listaImagens");
-const button = document.querySelector("#readmore");
+// const exibir = document.querySelector("#listaImagens");
+// const button = document.querySelector("#readmore");
 
-button.onclick = function () {
-    if (exibir.className == "open") { 
-      // ler menos
-      exibir.className = "";  
-      button.innerHTML = "Mostrar mais"; // Ativar o botão mostrar mais
-    } else {
-      // ler mais
-      exibir.className = "open"; // se a class estiver open
-      button.innerHTML = "Mostrar menos"; // Trocar o botão 
-    }
- };
+// button.onclick = function () {
+//     if (exibir.className == "open") { 
+//       // ler menos
+//       exibir.className = "";  
+//       button.innerHTML = "Mostrar mais"; // Ativar o botão mostrar mais
+//     } else {
+//       // ler mais
+//       exibir.className = "open"; // se a class estiver open
+//       button.innerHTML = "Mostrar menos"; // Trocar o botão 
+//     }
+//  };
 
+ function Mudarestado(el) {
+  const display = document.getElementById(el).style.display;
+  if(display == "none")
+      document.getElementById(el).style.display = 'block';
+  else
+      document.getElementById(el).style.display = 'none';
+}
 
 //  button.onclick = function () {
 //   if (exibir.className != "open") { 
